1916cdfa0984f73f5383f889948aba276420bf23
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-29 02:38:23 +0530

    create admin middleware

---
+++ backend/middleware/admin.middleware.js
@@ -0,0 +1,17 @@
+export const adminMiddleware = (req, res, next)=>{
+    try {
+        if (!req.user) {
+            return res.status(401).json({
+                message: "Unauthorized. Please login first."
+            })
+        }
+        
+        if(req.user.role !== 'admin'){
+            return res.status(403).json({message: "Access denied. Admins only."});
+        }
+
+        next();
+    } catch (error) {
+        return res.status(500).json({message: "Server error in admin authorization."});
+    }
+}
