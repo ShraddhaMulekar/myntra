7c901c53b45d7471aedce173d52bb5988d788333
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-29 00:24:21 +0530

    use cors to unblock browser requests coming from a different origin.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express"
+import cors from "cors"
 import dotenv from "dotenv"
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use(cors())
 app.use(express.json())
 
 app.listen(port, ()=>{
