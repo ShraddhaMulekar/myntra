8864616d1e845f6227d6c6fea243b4ffd409334a
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-29 00:40:51 +0530

    user schema and model created!

---
+++ backend/models/user.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    createdAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true },
+  { versionKey: false },
+);
+
+export const UserModel = mongoose.model("user", userSchema)
