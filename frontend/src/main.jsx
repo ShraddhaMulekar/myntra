51b50db2c0a0ce2fb31d47475e363f9e43f0b0a0
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-29 03:30:54 +0530

    add router shell in main file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthContextProvider } from './context/AuthContext.jsx'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <AuthContextProvider>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </AuthContextProvider>,
 )
