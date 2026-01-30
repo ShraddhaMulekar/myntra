9bfe2600ad421e1fcb6d90c80cf8cfafbb10d753
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-30 16:37:28 +0530

    create product model

---
+++ backend/models/product.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    images: {
+      type: [String],
+      default: [],
+    },
+    sizes: {
+      type: [String],
+      default: [],
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    description: {
+      type: String,
+    },
+  },
+  {
+    versionkey: true,
+    timestamps: true,
+  },
+);
+
+export const ProductModel = mongoose.connect("product", productSchema)
