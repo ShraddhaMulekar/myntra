9336146e2fab665082a804d0e4dbfa684ad07549
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-01-30 18:13:25 +0530

    add admin middleware for protected for only created for admin

--- backend/routes/product/product.route.js
+++ backend/routes/product/product.route.js
@@ -2,9 +2,10 @@ import express from "express"
 import { getProducts } from "../../pages/products/getProducts.page.js"
 import { getProductById } from "../../pages/products/getProductById.page.js"
 import { createProduct } from "../../pages/products/createProduct.page.js"
+import { adminMiddleware } from "../../middleware/admin.middleware.js"
 
 export const productRouter = express.Router()
 
 productRouter.get("/", getProducts)             // GET /products/
 productRouter.get("/:id", getProductById)       // GET /products/:id
-productRouter.post("/create", createProduct)    // POST /products/create
+productRouter.post("/create", adminMiddleware, createProduct)    // POST /products/create
