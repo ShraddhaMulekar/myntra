b7e77f3a8b7eeb452ee5832d33929f2e334576f9
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-02 20:41:15 +0530

    add get products by id fetch api in frontend

--- frontend/src/api/productApi.js
+++ frontend/src/api/productApi.js
@@ -4,9 +4,16 @@ export const getProducts = async(query="")=>{
     try {
         const res = await fetch(`${api}/products${query}`)
         return res.json()
-
     } catch (error) {
         console.log("error in get products api:", error)
     }
 }
 
+export const getProductsById = async(id)=>{
+    try {
+        const res = await fetch(`${api}/products/${id}`)
+        return res.json()
+    } catch (error) {
+        console.log("error in get products api:", error)
+    }
+}
