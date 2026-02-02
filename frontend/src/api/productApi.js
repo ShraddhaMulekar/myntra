bead72575da2d36f4898e680ae3b3e64791cd84b
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-02 20:46:19 +0530

    add fetch api for create new product using post method as well provide token in frontend side

--- frontend/src/api/productApi.js
+++ frontend/src/api/productApi.js
@@ -14,6 +14,22 @@ export const getProductsById = async(id)=>{
         const res = await fetch(`${api}/products/${id}`)
         return res.json()
     } catch (error) {
-        console.log("error in get products api:", error)
+        console.log("error in get products by id api:", error)
+    }
+}
+
+export const addProduct  = async(product, token)=>{
+    try {
+        const res = await fetch(`${api}/products/create`,{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json",
+                Authorization : `Bearer ${token}`
+            },
+            body : JSON.stringify(product)
+        })
+        return res.json()
+    } catch (error) {
+        console.log("error in create products api:", error)
     }
 }
