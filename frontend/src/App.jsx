31edeca1f1b569883cf6bb216e8b725b6d1d2146
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-02 21:33:14 +0530

    create route for home, product detail and add productes pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
-import { Routes, Route } from 'react-router-dom'
-import Register from './pages/Register'
-import { Login } from './pages/login'
+import { Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import { Login } from "./pages/login";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import AddProduct from "./pages/Admin/AddProduct";
 
 function App() {
-
   return (
     <Routes>
-      <Route path='/register' element={<Register />} />
-      <Route path='/login' element={<Login />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/products/:id" element={<ProductDetails />} />
+      <Route path="/admin/add-product" element={<AddProduct />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
